e0ff53735522ac31ce7fd10305f39db25248a07e
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-28 12:13:42 +0530

    option menu food

---
+++ FoodMenu/foodMenu.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+void display_time_menu();
+void displayMenu();
+void get_user_time();
+int main()
+{
+    int option;
+    do
+    {
+        printf("\n------------FOOD ORDER SYSTEM--------------\n");
+        printf("1. Display Menu\n");
+        printf("2. Order Food\n");
+        printf("3. Order History\n");
+        printf("4. Exit\n");
+        printf("--------------------------------------------\n");
+
+        scanf("%d", &option);
+
+    } while (option != 4);
+    return 0;
+}
+
+void displayMenu()
+{
+}
+
+void display_time_menu()
+{
+
+    printf("---------------Time--------------\n");
+    printf("Say Good Moring (1)\n");
+    printf("Say Good Afternoon (2)\n");
+    printf("Say Good Evening (3)\n");
+    printf("---------------------------------\n");
+}
+
+void get_user_time()
+{
+    int time = printf("Enter the Time:");
+    switch (time)
+    {
+    case 1:
+        printf("\nGood Morning");
+        break;
+    case 2:
+        printf("\nGood Afternoon");
+        break;
+    case 3:
+        printf("\nGood Evening");
+
+    default:
+        break;
+    }
+}
