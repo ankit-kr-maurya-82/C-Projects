25a934c78ccf73e8ba6d02dde1ffdb83266141d2
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-28 17:14:38 +0530

    bug and fix

--- FoodMenu/foodMenu.c
+++ FoodMenu/foodMenu.c
@@ -2,30 +2,46 @@
 void display_time_menu();
 void displayMenu();
 void get_user_time();
-int main()
-{
+int main(){
     int option;
     do
     {
-        printf("\n------------FOOD ORDER SYSTEM--------------\n");
+        printf("------------FOOD ORDER SYSTEM--------------\n");
         printf("1. Display Menu\n");
         printf("2. Order Food\n");
         printf("3. Order History\n");
         printf("4. Exit\n");
         printf("--------------------------------------------\n");
 
+        printf("Enter your choice: ");
         scanf("%d", &option);
 
+        switch (option)
+        {
+        case 1 :
+            printf("code 1");
+            break;
+        case 2 :
+            printf("code 2");
+            break;
+        case 3 :
+            printf("code 3");
+            break;
+        case 4 :
+            printf("code 4");
+            break;
+        
+        default:
+            printf("Invalid choice!!");
+        }
     } while (option != 4);
     return 0;
 }
 
-void displayMenu()
-{
+void displayMenu(){
 }
 
-void display_time_menu()
-{
+void display_time_menu(){
 
     printf("---------------Time--------------\n");
     printf("Say Good Moring (1)\n");
@@ -34,8 +50,7 @@ void display_time_menu()
     printf("---------------------------------\n");
 }
 
-void get_user_time()
-{
+void get_user_time(){
     int time = printf("Enter the Time:");
     switch (time)
     {
