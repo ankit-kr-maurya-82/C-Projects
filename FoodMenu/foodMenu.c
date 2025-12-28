f48911b636e3849d7b3244b3ae6d0700f6da0e3f
AK Maurya <143257696+ankit-kr-maurya-82@users.noreply.github.com>
2025-12-28 21:00:10 +0530

    run in Linux

--- FoodMenu/foodMenu.c
+++ FoodMenu/foodMenu.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-// function declarations
+// Menu-Driven Program by Function
 void display_time_menu();
 void displayMenu();
 void get_user_time();
+void foodItemMorning();
+void Drinks(int selectDrinks);
 
 int main() {
     int user_choice;
@@ -41,7 +43,7 @@ int main() {
     return 0;
 }
 
-// food menu (dummy)
+// food menu
 void displayMenu() {
     printf("\nFood Menu Coming Soon...\n");
 }
@@ -55,7 +57,7 @@ void display_time_menu() {
     printf("----------------------------------------\n");
 }
 
-// greeting logic
+//  logic
 void get_user_time() {
     int time;
     printf("Enter your choice: ");
@@ -64,6 +66,8 @@ void get_user_time() {
     switch (time) {
         case 1:
             printf("Good Morning \n");
+            printf("Food Item \n");
+            foodItemMorning();
             break;
         case 2:
             printf("Good Afternoon \n");
@@ -73,5 +77,34 @@ void get_user_time() {
             break;
         default:
             printf("Invalid time choice!\n");
+    }
+}
+
+void foodItemMorning(){
+    printf("Drinks (1)\n");
+    printf("Snacks (2)\n");
+
+    int selectUser;
+    printf("enter item: ");
+    scanf("%d", &selectUser);
+    if(selectUser==1){
+        printf("Hot Drinks (1)\n");
+        printf("Cold Drinks (2)\n");
+
+        int selectDrinks;
+        printf("Select Drink: ");
+        scanf("%d", &selectDrinks);
+
+         Drinks(selectDrinks);
+
+    }
+}
+
+// drinks functions
+void Drinks(int selectDrinks){
+    if(selectDrinks == 1){
+        printf("Coca Cola");
+    }else if(selectDrinks == 2){
+        printf("Chai");
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 C Projects
 git remote add origin https://github.com/ankit-kr-maurya-82/C-Projects.git
+
+## Linux
+└─$ gcc foodMenu.c<br>
+./a.out<br>
